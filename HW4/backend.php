65a306959268b69aef85b3b599b75554f0d3cf8d
Rami Alhussein <Ramialhussein98@gmail.com>
2020-04-09 21:23:43 -0400

    updating hws

--- HW4/backend.php
+++ HW4/backend.php
@@ -4,6 +4,8 @@
     $password = "admin";
     $dbname = "csit101";
     $errors = array();
+    $fname = "";
+    $lname = "";
 
     // Create connection
     $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -29,26 +31,36 @@
             if(mysqli_num_rows($results) == 1){
                 $fname = $userInfo['firstname'];
                 $lname = $userInfo['lastname'];
+                
+                $_SESSION['fname'] = $fname;
+                $_SESSION['lname'] = $lname;
+            } else {
+                array_push($errors, '1');
             }
         }
     }
-
-    
-    
-
-    
-    
-    
-    
-    
-    
-    
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    
-    $sql = "SELECT * from administrators Where email=".$email." and password=".$password;
-    if(mysqli_query($conn, $sql)){
-        echo 'you logged in';
-    }
-    
 ?>
+<html>
+    <body>
+        <?php if(count($errors) == 0){ ?>
+            <h1><?php echo 'Good job! '.$fname.' '.$lname.', successfully logged in!!' ?></h1>
+        <?php } else { ?>
+            <form action="backend.php" method="post">
+                <h1>Welcome to CSIT website</h1>
+                <h2>Please Login</h2>
+                <label>Email: </label>
+                <input type="email" name="email" id="email" required="required" />
+                <br />
+                           
+                <label>Password: </label>
+                <input type="password" name="password" id="password" required="required" />
+                <br />
+                           
+                <input type="submit"  value="Login" name="login_user" />
+                           
+                           
+                <p>Error: Invalid credintials, you must corretly login to view this site</p>
+            </form>
+    <?php }; ?>
+    </body>
+</html>
